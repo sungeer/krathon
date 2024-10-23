f20bb87d1310ee91b9ea9192f3f7a961dc23b3fc
wangxun <1013296326@qq.com>
2024-10-23 21:54:44 +0800

    up do redis pool

--- krathon/tasks/huey_instance.py
+++ krathon/tasks/huey_instance.py
@@ -4,14 +4,14 @@ from redis import ConnectionPool
 from krathon.configs import settings
 from krathon.tasks.log_config import logger  # 配置日志记录器
 
-connection_pool = ConnectionPool(
+redis_pool = ConnectionPool(
     host=settings.redis_host,
     port=6379,
     password=settings.redis_passwd,
     db=0
 )
 
-huey = RedisHuey(settings.app_name, connection_pool=connection_pool, blocking=True)
+huey = RedisHuey(settings.app_name, connection_pool=redis_pool, blocking=True)
 
 # 在消费者 启动时 加载 定时任务
 from krathon.tasks import scheduled_tasks
