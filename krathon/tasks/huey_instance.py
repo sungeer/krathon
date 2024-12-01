e4a8571c87435017ba16892491217033a8a457c2
wangxun <1013296326@qq.com>
2024-12-01 14:59:50 +0800

    忽略特定警告

--- krathon/tasks/huey_instance.py
+++ krathon/tasks/huey_instance.py
@@ -2,7 +2,7 @@ from huey import RedisHuey
 from redis import ConnectionPool
 
 from krathon.configs import settings
-from krathon.tasks.log_config import logger  # 配置日志记录器
+from krathon.tasks.log_config import logger  # noqa 配置日志记录器
 
 redis_pool = ConnectionPool(
     host=settings.redis_host,
@@ -14,4 +14,4 @@ redis_pool = ConnectionPool(
 huey = RedisHuey(settings.app_name, connection_pool=redis_pool, blocking=True)
 
 # 在消费者 启动时 加载 定时任务
-from krathon.tasks import scheduled_tasks
+from krathon.tasks import scheduled_tasks  # noqa
